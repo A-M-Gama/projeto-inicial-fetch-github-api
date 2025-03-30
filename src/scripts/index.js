527456258a0f33da7c49f16030b9845e30bb0162
Arthur Gama <arthur.m.gama99@gmail.com>
2025-03-30 15:18:12 -0300

    inserida validações de campo vazio e usuário não existente

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,10 @@ import { screen } from "/src/scripts/objects/screen.js"
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
+
+    if(validateEmptyInput(userName)) return
     getUserData(userName)
+    
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
@@ -15,12 +18,19 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const isEnterKeyPressed = key === 13
 
     if(isEnterKeyPressed){
+        if(validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
 
 async function getUserData(userName){
     const userResponse = await getUser(userName)
+
+    if(userResponse.message === "Not Found"){
+        screen.renderNotFound()
+        return
+    }
+
     const repositoriesResponse = await getRepositories(userName)
     
     user.setInfo(userResponse)
@@ -28,4 +38,11 @@ async function getUserData(userName){
     screen.renderUser(user)
 
     console.log(user)
+}
+
+function validateEmptyInput(userName){
+    if(userName.length === 0) {
+        alert('Preencha o campo com um nome de usuário do GitHub')
+        return true
+    }
 }
